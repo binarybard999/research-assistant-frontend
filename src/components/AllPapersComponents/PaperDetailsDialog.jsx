b744e6a065686d1f54be7ccccb86c48072cad3e2
Arshpreet Singh <loloflol1223@gmail.com>
2025-05-14 20:11:24 +0530

    minor fix

--- src/components/AllPapersComponents/PaperDetailsDialog.jsx
+++ src/components/AllPapersComponents/PaperDetailsDialog.jsx
@@ -26,7 +26,7 @@ const PaperDetailsDialog = ({
 
                 <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
 
-                <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
+                <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6 relative z-10">
                     <div>
                         <div className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-blue-100">
                             <FileText className="h-6 w-6 text-blue-600" />
